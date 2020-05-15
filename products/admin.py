966e20cc90c1d986af3534cab974fbc2c13c5786
Marco Bossi <marco.bossi@reti.it>
2020-05-15 17:58:49 +0200

    Inizialize project

---
+++ products/admin.py
@@ -0,0 +1,12 @@
+from .models import Product, Offer
+from django.contrib import admin
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code','discount')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','stock')
+
+# Register your models here.
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Offer,OfferAdmin)
